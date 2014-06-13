c6c0ddf005222abf107b3885c564aff4132c55d9
chankamwo <470042768@qq.com>
2014-06-13 13:10:30 +0800

    merge

--- Classes/StealScoreEffect.cpp
+++ Classes/StealScoreEffect.cpp
@@ -7,11 +7,16 @@
 #include "StealScoreEffect.h"
 #include "PlayScene.h"
 
+
 StealScoreEffect::StealScoreEffect()
 {
 }
 
 void StealScoreEffect::work( SnakeBase* snake )
 {
-	
+	auto snakes = dynamic_cast<PlayScene*>( snake->getScene() )->getSnakes();
+	auto enemy = snakes[0] == snake? snakes[1] : snakes[0];
+	auto delta = enemy->getScore() / 5;
+	enemy->setScore(enemy->getScore() - delta);
+	snake->setScore(snake->getScore() + delta);
 }
